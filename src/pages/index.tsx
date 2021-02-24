71ed0e33c0e5795f1167f7965996a3c4dfd0bd15
MatheusPMC <matheus.pmc@hotmail.com>
2021-02-24 17:48:43 -0300

    Next estruturado e Barra de exp criada

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import { ExperienceBar } from "../components/ExperirenceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+
+      <ExperienceBar />
+    </div>
+  )
+}
